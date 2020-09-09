41b723173dcf2aa1a1070e69f8c594e4970c50d1
wataru0 <wataru.tv.nanao@gmail.com>
2020-09-09 14:22:01 +0900

    initial commit

---
+++ intern_hackathon/Screen/EventList/EventListCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventListCell.swift
+//  intern_hackathon
+//
+//  Created by 岡本航昇 on 2020/09/09.
+//  Copyright © 2020 caraquri. All rights reserved.
+//
+
+import UIKit
+
+class EventListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ intern_hackathon/Screen/EventList/EventListViewController.swift
@@ -0,0 +1,10 @@
+//
+//  EventListViewController.swift
+//  intern_hackathon
+//
+//  Created by 岡本航昇 on 2020/09/09.
+//  Copyright © 2020 caraquri. All rights reserved.
+//
+
+import UIKit
+
